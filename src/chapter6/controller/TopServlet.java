f1fd0011c18540ba15e0aef4f488ad9d7b5eddab
ogihara_shoei <ogihara.shoei@alhinc.jp>
2026-06-15 16:13:32 +0900

    6/15 実践課題②

--- src/chapter6/controller/TopServlet.java
+++ src/chapter6/controller/TopServlet.java
@@ -18,34 +18,42 @@ import chapter6.service.MessageService;
 @WebServlet(urlPatterns = { "/index.jsp" })
 public class TopServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    /**
-    * ロガーインスタンスの生成
-    */
-    Logger log = Logger.getLogger("twitter");
+	/**
+	* ロガーインスタンスの生成
+	*/
+	Logger log = Logger.getLogger("twitter");
 
-    /**
-    * デフォルトコンストラクタ
-    * アプリケーションの初期化を実施する。
-    */
-    public TopServlet() {
-        InitApplication application = InitApplication.getInstance();
-        application.init();
-    }
+	/**
+	* デフォルトコンストラクタ
+	* アプリケーションの初期化を実施する。
+	*/
+	public TopServlet() {
+	    InitApplication application = InitApplication.getInstance();
+	    application.init();
+	}
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws IOException, ServletException {
 
-	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
-	  boolean isShowMessageForm = false;
-      User user = (User) request.getSession().getAttribute("loginUser");
-      if (user != null) {
-          isShowMessageForm = true;
-      }
-      List<UserMessage> messages = new MessageService().select();
-      request.setAttribute("messages", messages);
-	  request.setAttribute("isShowMessageForm", isShowMessageForm);
-	  request.getRequestDispatcher("/top.jsp").forward(request, response);
-    }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+
+		log.info(new Object(){}.getClass().getEnclosingClass().getName() +
+		" : " + new Object(){}.getClass().getEnclosingMethod().getName());
+		boolean isShowMessageForm = false;
+		User user = (User) request.getSession().getAttribute("loginUser");
+		if (user != null) {
+			isShowMessageForm = true;
+		}
+		/*
+		* String型のuser_idの値をrequest.getParameter("user_id")で
+		* JSPから受け取るように設定
+		* MessageServiceのselectに引数としてString型のuser_idを追加
+		*/
+		String userId = request.getParameter("user_id");
+		List<UserMessage> messages = new MessageService().select(userId);
+
+		request.setAttribute("messages", messages);
+		request.setAttribute("isShowMessageForm", isShowMessageForm);
+		request.getRequestDispatcher("/top.jsp").forward(request, response);
+	}
 }
--- src/chapter6/service/MessageService.java
+++ src/chapter6/service/MessageService.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.Message;
 import chapter6.beans.UserMessage;
 import chapter6.dao.MessageDao;
@@ -16,67 +18,84 @@ import chapter6.logging.InitApplication;
 
 public class MessageService {
 
+	/**
+	* ロガーインスタンスの生成
+	*/
+	Logger log = Logger.getLogger("twitter");
 
-    /**
-    * ロガーインスタンスの生成
-    */
-    Logger log = Logger.getLogger("twitter");
+	/**
+	* デフォルトコンストラクタ
+	* アプリケーションの初期化を実施する。
+	*/
+	public MessageService() {
+		InitApplication application = InitApplication.getInstance();
+		application.init();
+	}
 
-    /**
-    * デフォルトコンストラクタ
-    * アプリケーションの初期化を実施する。
-    */
-    public MessageService() {
-        InitApplication application = InitApplication.getInstance();
-        application.init();
-    }
 
-    public void insert(Message message) {
+	public void insert(Message message) {
 
-	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+		log.info(new Object(){}.getClass().getEnclosingClass().getName() +
+		" : " + new Object(){}.getClass().getEnclosingMethod().getName());
 
-        Connection connection = null;
-        try {
-            connection = getConnection();
-            new MessageDao().insert(connection, message);
-            commit(connection);
-        } catch (RuntimeException e) {
-            rollback(connection);
-		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-            throw e;
-        } catch (Error e) {
-            rollback(connection);
-		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-            throw e;
-        } finally {
-            close(connection);
-        }
-    }
-    public List<UserMessage> select() {
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			new MessageDao().insert(connection, message);
+			commit(connection);
+		} catch (RuntimeException e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
 
-  	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-          " : " + new Object(){}.getClass().getEnclosingMethod().getName());
 
-          final int LIMIT_NUM = 1000;
+	/*
+	 * selectの引数にString型のuserIdを追加
+	 */
+	public List<UserMessage> select(String userId) {
+		final int LIMIT_NUM = 1000;
 
-          Connection connection = null;
-          try {
-              connection = getConnection();
-              List<UserMessage> messages = new UserMessageDao().select(connection, LIMIT_NUM);
-              commit(connection);
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			/*
+			* idをnullで初期化
+			* ServletからuserIdの値が渡ってきていたら
+			* 整数型に型変換し、idに代入
+			*/
+			Integer id = null;
+			if(!StringUtils.isEmpty(userId)) {
+				id = Integer.parseInt(userId);
+			}
 
-              return messages;
-          } catch (RuntimeException e) {
-              rollback(connection);
-              log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-              throw e;
-          } catch (Error e) {
-              rollback(connection);
-              log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-              throw e;
-          } finally {
-              close(connection);
-          }
-      }
- }
+			/*
+			* messageDao.selectに引数としてInteger型のidを追加
+			* idがnullだったら全件取得する
+			* idがnull以外だったら、その値に対応するユーザーIDの投稿を取得する
+			*/
+			List<UserMessage> messages = new UserMessageDao().select(connection, id, LIMIT_NUM);
+
+			commit(connection);
+
+			return messages;
+		} catch (RuntimeException e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
+}
